19941095fdeeda3396d764c1277d709df4425091
Paolo D'Incau <paolo.dincau@xpeppers.com>
2015-10-26 15:07:11 +0100

    Improving formatting

--- app/src/main/java/com/xpeppers/linkingcommerce/merchantdashboard/models/order/OrderStatusConverter.java
+++ app/src/main/java/com/xpeppers/linkingcommerce/merchantdashboard/models/order/OrderStatusConverter.java
@@ -2,10 +2,10 @@ package com.xpeppers.linkingcommerce.merchantdashboard.models.order;
 
 public class OrderStatusConverter {
 
-    public static OrderStatus orderStatusFrom(String statusString){
-        try{
+    public static OrderStatus orderStatusFrom(String statusString) {
+        try {
             return OrderStatus.valueOf(statusString.toUpperCase());
-        }catch (Exception ex){
+        } catch (Exception ex){
             return OrderStatus.UNKNOW;
         }
     }
